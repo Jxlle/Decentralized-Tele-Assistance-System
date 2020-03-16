c80dcb4c30e0a63ee40b1060f9a64312faa274f5
Jelle Van De Sijpe <jelle.vandesijpe@gmail.com>
2020-03-16 21:27:03 +0100

    Fixed protocol that does nothing

--- TeleAssistanceSystem/src/tas/mape/communication/protocol/PlannerProtocolDoNothing.java
+++ TeleAssistanceSystem/src/tas/mape/communication/protocol/PlannerProtocolDoNothing.java
@@ -4,7 +4,9 @@ import tas.mape.communication.message.PlannerMessage;
 import tas.mape.planner.Planner;
 
 /**
- * Class representing a planner protocol that does nothing. 
+ * Class representing a planner protocol that does nothing with the communication features.
+ * The protocol just sends a confirmation to the other entity and the other entity ends the protocol as if
+ * both entities never communicated. 
  * 
  * @author Jelle Van De Sijpe (jelle.vandesijpe@student.kuleuven.be)
  *
@@ -13,12 +15,32 @@ import tas.mape.planner.Planner;
 public class PlannerProtocolDoNothing extends PlannerTwoComponentProtocol {
 
 	@Override
-	protected void InitializeAndSendFirstMessage(int startIndex, int... receiverIndices) {
-		// Do nothing		
-	}
-
-	@Override
 	public void receiveAndHandleMessage(PlannerMessage message, Planner receiver) {
-		// Messages are never handled
+	
+		String messageType = message.getType();
+		
+		switch(messageType) {
+		
+			// First offer has been received
+			case "FIRST_OFFER":
+				PlannerMessage response = new PlannerMessage(messageID, message.getSenderEndpoint(), receiver.getEndpoint(), "ACCEPTED_OFFER", null);
+				receiver.setCurrentServiceCombination(receiver.getAvailableServiceCombinations().get(0));
+				receiver.finishedProtocol();
+				
+				// Increase message ID
+				messageID++;
+				receiver.sendMessage(response);	
+				
+			// Offer has been accepted, stop protocol
+			case "ACCEPTED_OFFER":
+				receiver.setCurrentServiceCombination(receiver.getAvailableServiceCombinations().get(0));
+				receiver.finishedProtocol();
+				resetProtocol();
+				break;
+				
+			// Exception state
+			default:
+				throw new IllegalStateException("The received message has a type that cannot be processed! Type: " + messageType);
+		}
 	}
 }
